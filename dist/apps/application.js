8580ffcbabd41b9c1904b98a9628c03d336b1d8d
Jon Burns <jonburns10@gmail.com>
2016-12-20 16:16:40 -0700

    update to app.js file, added new form tokens for all forms

--- dist/apps/application.js
+++ dist/apps/application.js
@@ -1,19 +1,42 @@
-$(document).ready(function(){
-
-	// $(".main-menu").delay("500").fadeIn("slow");
-	$("#fade-title").delay("700").fadeIn("slow");
-	// Slider jQuery
-	$('.number-box').delay('700').fadeIn('slow');
-	//
+document.addEventListener('DOMContentLoaded', function () {
 	$('.sliderA').slick({
 		dots: true,
 		slidesToShow: 1,
 		speed: 1000,
 		adaptiveHeight: true,
-		autoplay: false,
+		autoplay: true,
 		autoplaySpeed: 5000,
 		arrows: false
-	})
+	});
+	var form = document.querySelector('.form');
+	if (form) {
+		Astatine.submit({
+			query: form,
+			method: 'post',
+			responseType: 'json',
+			action: 'https://www.enformed.io/hab8voqf',
+			prepare: function (data, resolve, reject) {
+				// data['*default_email'] = 'alex.steven.elias@gmail.com';
+				// data['*cc'] = 'aelias@webcabdesign.com';
+				if (!data['*default_email']) data['*default_email'] = 'admin@agapehospiceaz.com';
+				// if (!data['*cc']) data['*cc'] = 'jburns@webcabdesign.com';
+
+			resolve(data);
+			},
+			complete: function (error, success) {
+				var response = document.querySelector('.response');
+				if (error) {
+					console.log(error);
+					response.style.color = 'red';
+					response.innerText = 'Error Plese See Console';
+				} else {
+					form.style.display = 'none';
+					response.style.color = '#6d387a';
+					response.innerText = 'Form Is Submitted';
+				}
+			}
+		});
+	}
 });
 
 $(document).foundation();
